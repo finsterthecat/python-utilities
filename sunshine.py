115a9ae8a64b5b14a5d005c339b771937def929e
Tony Brouwer <tonybrouwer@gmail.com>
2023-04-13 11:34:26 -0400

    Support a regex argument

--- sunshine.py
+++ sunshine.py
@@ -11,13 +11,17 @@ parser = argparse.ArgumentParser(
     ' Send matched records to stdout, which you can redirect to a file.')
 parser.add_argument('input', help='the sunshine csv file')
 parser.add_argument("--filter", nargs="*", type=str, default=[], help='list of keywords to match in Job Title column')
+parser.add_argument("--regex", type=str, help='regex to match Job Title, overrides filter if present')
+parser.add_argument("--xregex", type=str, help='regex to exclude rows where they match Job Title')
 args = parser.parse_args()
 
 rows = fileio.read_csv_file(args.input)
 
-ALL = "(Equity)|(Diversity)|(Inclusion)|(Anti-Racism)|(Indigenous)|(Equality)|(Rights)|(Gender)"
+ALL = "(Equity)|(Diversity)|(Inclusion)|(Racism)|(Equality)|(Rights)|(Gender)"
 
-if args.filter == []:
+if args.regex != None:
+    filter = args.regex
+elif args.filter == []:
     filter = ALL
 else:
     filter = "(" + ")|(".join(args.filter) + ")"
@@ -38,4 +42,7 @@ for row in rows:
         total += float(row["Salary"])
         writer.writerow(row)
 
-sys.stderr.write(f"{count} matches. Total is {total:9,.2f}. Average {(total/count):9,.2f}\n")
+sys.stderr.write(f"{count} matches. Total is {total:9,.2f}.")
+if count > 0:
+    sys.stderr.write(f"Average {(total/count):9,.2f}")
+sys.stderr.write("\n")
